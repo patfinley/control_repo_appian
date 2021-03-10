a21da6a0ecc18aa22a01e68f24582231bafd0655
patfinley <79937428+patfinley@users.noreply.github.com>
2021-03-10 10:44:04 -0500

    Update init.pp

--- site/appian/manifests/init.pp
+++ site/appian/manifests/init.pp
@@ -1,17 +1,29 @@
 class  appian (
+  $database_nickname     = 'jdbc/AppianDS',
   $database_name         = 'appian_203',
   $username              = 'admin',
   $password              = 'password',
 )
 {
    $appian_source_hash = {
-    'database_name'  => $database_name,
-    'username'       => $username,
-    'password'       => $password,
+    'database_nickname' => $database_nickname,
+    'database_name'     => $database_name,
+    'username'          => $username,
+    'password'          => $password,
 }
 
  file{ '/etc/target.txt':
    content => epp('appian/target.txt.epp', $appian_source_hash)
+   before  => File['/etc/custom.properties']  
+  }
+  $appian_source_hash = {
+    'database_nickname' => $database_nickname,
+    'fqdn'              => $facts['networking']['fqdn'],
+    'company_domain'    => $company_domain,
+    
+} 
+ file{ '/etc/cusotm.properties':
+   content => epp('appian/custom.properties.epp', $appian_properties_hash)
   }
 
 

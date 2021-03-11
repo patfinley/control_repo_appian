9878e9bc970817680ce31023ede32c48b0c23601
patfinley <79937428+patfinley@users.noreply.github.com>
2021-03-11 15:20:34 -0500

    Update app.pp

--- site/profile/manifests/appian/app.pp
+++ site/profile/manifests/appian/app.pp
@@ -1,9 +1,15 @@
 class profile::appian::app {
   class { 'appian':
-    database_nickname      => lookup('profile::appian::app::database_nickname'),
-    database_name          => lookup('profile::appian::app::database_name'),
-    username               => lookup('profile::appian::app::username'),
-    password               => lookup('profile::appian::app::password'),
-    company_domain         => lookup('profile::appian::app::company_domain'),
+    database_nickname        => lookup('profile::appian::app::database_nickname'),
+    database_name            => lookup('profile::appian::app::database_name'),
+    username                 => lookup('profile::appian::app::username'),
+    password                 => lookup('profile::appian::app::password'),
+    company_domain           => lookup('profile::appian::app::company_domain'),
+    directory_appian_home    => lookup('profile::appian::app::directory_appian_home'),
+    directory_appian_repo    => lookup('profile::appian::app::directory_appian_repo'),
+    directory_appian_backup  => lookup('profile::appian::app::directory_appian_backup'),
+    environment              => lookup('profile::appian::app::environment'),
+
+
     }
   }
